bf94e09b4ae4672d95e53bdd880fec7b4ee46fb2
Panos Nikolaou <38185286+PanosNikolaou@users.noreply.github.com>
2018-04-12 18:57:49 +0200

    Update and rename invma.pas to inversemat.pas

--- inversemat.pas
+++ inversemat.pas
@@ -1,26 +1,16 @@
 {
-ENVIRONMENTAL MODELLING SS 2017/2018
-PANAGIOTIS NIKOLAOU
-FINDING AN INVERSE MATRIX OF A 3X3 TABLE
+FINDING AN INVERSE MATRIX OF A 3X3 MATRIX
 USING THE FORMULA A^-1=1/DET(A)*ADJ(A)
-SOLVE LINEAR SYSTEM BY
-GAUSSIAN ELIMINATION WITH PARTIAL PIVOTING
-
-Ax = b
-roundoff error
-gauss elimination
 }
 
-PROGRAM invma;
+PROGRAM inversemat;
 
 TYPE
   TMatrix  = ARRAY[1..3, 1..3] OF Real;
-  TVector  = ARRAY[1..3] OF Real;
 
 VAR
   A,T: TMatrix;
   ADJ,AINV : TMatrix;
-  B,X : TVector;
   N,i,j : Integer;
   endch: Char;
   DeterminantVAR: Real;
@@ -52,8 +42,6 @@ BEGIN
   WriteLn
 END;
 
-PROCEDURE READBDATA(VAR B: TVector; N: Integer);
-
 VAR
   I, J: Integer;
 
@@ -65,8 +53,8 @@ BEGIN
           Write( I, ': ');
           ReadLn( B[I])
         END;
-  //WriteLn;
-  //WriteLn( 'INITIAL MATRIX');
+  WriteLn;
+  WriteLn( 'INITIAL MATRIX');
   FOR I:= 1 TO N  DO
       BEGIN
            Write( B[I]:7:4, '  ');
@@ -74,74 +62,6 @@ BEGIN
       END;
   WriteLn
 END;
-
-// Solve A x = b
-PROCEDURE gauss (VAR A : TMatrix; VAR b,x : TVector);
-VAR rowx : integer;
-    i, j, k, n, m : integer;
-    amax, xfac, temp, temp1 : double;
-BEGIN
-  n := 3;
-  rowx := 0;  // Keep count of the row interchanges
-  //n := A.r;
-
-  WriteLn( 'Forward elimination started');
-
-  FOR k := 1 TO n - 1 DO
-      BEGIN
-      amax := abs (A[k,k]);
-      m := k;
-      // Find the row with largest pivot
-      FOR i := k + 1 TO n DO
-          BEGIN
-          xfac := abs (A[i,k]);
-          IF xfac > amax THEN
-             BEGIN
-             amax := xfac;
-             m := i;
-             END;
-          END;
-
-      IF m <> k THEN
-         BEGIN  // Row interchanges
-         rowx := rowx+1;
-         temp1 := b[k];
-         b[k] := b[m];
-         b[m]  := temp1;
-         FOR j := k TO n DO
-             BEGIN
-             temp := a[k,j];
-             a[k,j] := a[m,j];
-             a[m,j] := temp;
-             END;
-      END;
-
-      //forward elimination
-
-      FOR i := k+1 TO n DO
-          BEGIN
-          xfac := a[i, k]/a[k, k];
-          FOR j := k+1 TO n DO
-              a[i,j] := a[i,j]-xfac*a[k,j];
-          b[i] := b[i] - xfac*b[k]
-          END;
-      END;
-
- WriteLn( 'Back substitution started');
-
-  // Back substitution
-  FOR j := 1 TO n DO
-      BEGIN
-      k := n-j + 1;
-      x[k] := b[k];
-      FOR i := k+1 TO n DO
-          BEGIN
-          x[k] := x[k] - a[k,i]*x[i];
-          END;
-  x[k] := x[k]/a[k,k];
-  END;
-END;
-
 
 PROCEDURE Cofactor (VAR TA:TMatrix;A: TMatrix);
 
@@ -195,28 +115,8 @@ BEGIN
          T[i,j]:= i * j;
     END;
 
-    // INITIALIZE MATRIX B AND X
-    BEGIN
-     FOR i:= 1 TO N DO
-         B[i]:= i;
-         X[i]:= i;
-    END;
-
-    // Test matrices
-
-    // 12  2  2 = 3
-    // 3   2  3 = 2
-    // 2   1  2 = 1
-
-    //x1 = 0.1
-    //x2 = 1
-    //x3 = -0.1
-
     WriteLn( 'INSERT VALUES FOR MATRIX A');
     READDATA(A,N);
-
-    WriteLn( 'INSERT VALUES FOR VECTOR B');
-    READBDATA(B,N);
 
     Cofactor(T,A);
 
@@ -267,29 +167,7 @@ BEGIN
           Write( AINV[I,J]:7:4, '  ');
         WriteLn();
       END;
-
-    WriteLn;
-    WriteLn( 'SOLVING SYSTEM AX=B BY GAUSSIAN ELIMINATION');
-
-    gauss(a,b,x);
-
-    WriteLn;
-    WriteLn( 'MATRIX A AFTER GAUSSIAN ELIMINATION');
-
-    FOR I:= 1 TO N  DO
-        BEGIN
-          FOR J:= 1 TO N DO
-            Write( A[I,J]:7:4, '  ');
-          WriteLn();
-        END;
-
-    WriteLn;
-    WriteLn( 'X SOLUTIONS');
-
-    FOR I:= 1 TO N  DO
-        Write( X[I]:7:4, '  ');
-
-    WriteLn;
+      
     WriteLn;
     WriteLn( 'PRESS ENTER TO TERMINATE');
     ReadLn(endch)
